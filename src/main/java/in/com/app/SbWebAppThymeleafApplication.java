50dbb431ea82b74178a257f5792ee20f532e6d08
kalidas1991 <kalidasd1991@gmail.com>
2022-06-04 13:48:08 +0530

    first commit

---
+++ src/main/java/in/com/app/SbWebAppThymeleafApplication.java
@@ -0,0 +1,13 @@
+package in.com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWebAppThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWebAppThymeleafApplication.class, args);
+	}
+
+}
